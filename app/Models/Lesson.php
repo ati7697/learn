2dddb6a0da6018c8222968d8e5bcf24d496ee6a2
Ati <atii.ahmadi76@gmail.com>
2022-09-07 11:23:21 +1000

    add soft delete, guarded to lesson model add relationship btw course and lesson

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Lesson extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
