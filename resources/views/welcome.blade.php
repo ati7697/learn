a2de02452f646262b83d6227e6f63ec64513a6d6
SethSharp <sesharp@outlook.com>
2022-09-13 12:15:22 +1000

    Test new branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title> Escape with Laravel </title>
+        <title> Testing branch push </title>
         @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
