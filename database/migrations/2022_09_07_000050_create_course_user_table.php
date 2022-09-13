29c67ccfd1a51be9e4fd0c7ad582e0102ae6e38d
SethSharp <sesharp@outlook.com>
2022-09-13 11:58:19 +1000

    Remove on

--- database/migrations/2022_09_07_000050_create_course_user_table.php
+++ database/migrations/2022_09_07_000050_create_course_user_table.php
@@ -11,7 +11,7 @@ return new class() extends Migration {
             $table->id();
             $table->foreignId('user_id');
             $table->foreignId('course_id');
-            $table->foreignId('current_lesson_id')->on('lessons');
+            $table->foreignId('current_lesson_id');
             $table->timestamps();
         });
     }
