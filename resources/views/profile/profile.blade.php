e40182e4a2f5efbd00b2fe13012b5fdf8b851f7d
Ati <atii.ahmadi76@gmail.com>
2022-09-28 16:03:16 +1000

    add edit link for editing profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -3,13 +3,16 @@
 <div class="w-screen text-white font-nav">
     <div class="flex flex-col justify-center items-center">
         <div class="rounded-full w-[150px] ml-2"> <img src="{{url('image/profile-removebg-preview.png')}}" ></div>
-        <div class="flex flex-col bg-gray-700 rounded-2xl justify-center p-4 w-48 text-center space-y-4 ">
-            <a href="" class="hover:bg-orange-400 rounded-2xl p-2">{user Name}</a>
-            <a href="" class="hover:bg-orange-400 rounded-2xl p-2">{user Email}</a>
+        <div class="flex flex-col bg-gray-700 rounded-2xl justify-center p-4 w-96 text-center space-y-4 ">
+            <a href="" class="hover:bg-orange-400 rounded-2xl p-2">{{$user->name}}</a>
+            <a href="" class="hover:bg-orange-400 rounded-2xl p-2">{{$user->email}}</a>
             <a href="" class="hover:bg-orange-400 rounded-2xl p-2">Courses</a>
             <a href="" class="hover:bg-orange-400 rounded-2xl p-2">Payment</a>
-            <a href="{{url('profile/edit')}}" class="hover:bg-orange-400 rounded-2xl p-2">Edit profile</a>
-            <a href="" class="hover:bg-orange-400 rounded-2xl p-2">Logout</a>
+            <a href="{{url('profile/{id}/edit')}}" class="hover:bg-orange-400 rounded-2xl p-2">Edit profile</a>
+            <form method="POST" action="{{ route('logout') }}" >
+                @csrf
+                <button class="hover:bg-orange-400 rounded-2xl p-2 px-6 w-full">Logout</button>
+            </form>
         </div>
     </div>
     </div>
