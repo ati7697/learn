4e690df285be34b3da7325a9ca77f29037347b53
TJeffery <30278647+TJeffery@users.noreply.github.com>
2022-10-05 11:32:01 +1000

    fixed alert issue and format

--- resources/js/Pages/Lessons/Show.vue
+++ resources/js/Pages/Lessons/Show.vue
@@ -44,7 +44,7 @@ const receivedJsonSaveFromUnity = async (e) => {
             game_state: e.detail.gamestate
         });
     } catch (e) {
-        window.alert("failed to add to database, error: ", +e);
+        window.alert("failed to add to database, error: " + e);
     }
 }
 
@@ -103,8 +103,12 @@ const requestLoadGameData = async (e) => {
                                 </div>
                             </div>
                         </div>
-                        <textarea v-model="challengeUserAnswer" type="text" id="large-input"
-                                  class="block  h-40 p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                        <textarea
+                            v-model="challengeUserAnswer"
+                            type="text"
+                            id="large-input"
+                            class="block  h-40 p-4 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
                         <div class="flex row-auto justify justify-center gap-5">
                             <button type="submit"
                                     class=" my-4 w-20 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
