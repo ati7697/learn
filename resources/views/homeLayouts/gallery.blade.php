5f077fd7076412b0d01894f63996b660520cc49f
ati7697 <88265864+ati7697@users.noreply.github.com>
2022-11-01 14:10:38 +1000

    remove courses image from gallery add description for larascape

--- resources/views/homeLayouts/gallery.blade.php
+++ resources/views/homeLayouts/gallery.blade.php
@@ -14,20 +14,21 @@
                     Browse Courses
                 </button>
             </div>
-        {{--    <div class="absolute bottom-20 left-16 text-black bg-white py-1 px-4 rounded-xl hover:bg-orange-400  font-light text-sm  text-center">--}}
-        {{--        <p>Beginner</p>--}}
-        {{--    </div>--}}
-        {{--    <div class="absolute bottom-12 left-16 text-white py-1 px-4 rounded-xl hover:bg-orange-400  font-light text-sm  text-center">--}}
-        {{--        <p>8 hours</p>--}}
-        {{--    </div>--}}
-        {{--    <div class="absolute bottom-4 left-16 text-white py-1 px-4 rounded-xl hover:bg-orange-400  font-light text-sm  text-center">--}}
-        {{--        <p>Certificate</p>--}}
-        {{--    </div>--}}
         </div>
     </div>
-    <div class="lg:flex lg:flex-row lg:justify-center md:flex md:flex-row md:justify-center sm:grid sm:grid-cols-2  mt-60">
-        <div class="m-4 group"><img src="{{url('image/gallaryp1.png')}}" class="lg:w-[200px] md:w-[300px] rounded-2xl group-hover:scale-150 transition ease-out duration-200 group-hover:brightness-50">
-            <div class="hidden group-hover:flex flex-col absolute -bottom-80 left-40 space-y-2 p-2 bg-gradient-to-b from-transparent via-black to-black w-60">
+    <div class="lg:flex lg:flex-row lg:justify-center md:flex md:flex-row md:justify-center sm:grid sm:grid-cols-2  mt-44 ">
+        <div class="m-4 group">
+            <div class="flex flex-row bg-gray-900 rounded-2xl w-max h-max">
+                <div> <img src="{{url('image/gallaryp1.png')}}" class="flex lg:w-[200px] md:w-[300px] rounded-2xl  ease-out duration-200 group-hover:brightness-50"></div>
+                <div class = "text-white  hidden group-hover:flex flex-col w-1/2 text-left ml-20 justify-center ">
+                    <h1 class="font-bold">Larascape</h1>
+                    <p>Game on! Explore our course selection <br> and elevate your coding skills to the next level!</p>
+                    <button type="button" class=" m-2 text-white bg-gradient-to-r from-orange-500 to-red-700 py-1 px-2 hover:px-4 transition-all ease-in rounded-xl  hover:bg-orange-400 text-sm  text-center">
+                        Play demo!
+                    </button>
+                </div>
+            </div>
+            <div class="hidden group-hover:flex flex-col absolute -bottom-52  space-y-1 p-2 bg-gradient-to-b from-transparent via-black to-black w-48">
                 <div class="bg-orange-600 py-1 px-2 rounded hover:bg-orange-400 text-white w-fit ">
                     <p>Laravel</p>
                 </div>
@@ -38,45 +39,12 @@
                     <p>Beginner</p>
                 </div>
             </div>
+
         </div>
-        <div class="m-4 group"><img src="{{url('image/gallaryp2.png')}}" class="lg:w-[200px] md:w-[300px] rounded-2xl group-hover:scale-150 transition ease-out duration-200 group-hover:brightness-50">
-            <div class="hidden group-hover:flex flex-col absolute -bottom-80 left-96 space-y-2 p-2 w-72 shadow shadow-lg shadow-black">
-                <div class="bg-orange-600 py-1 px-6 rounded hover:bg-orange-400 text-white w-fit ">
-                    <p>Vue</p>
-                </div>
-                <div class="text-gray-500  text-sm   hover:text-white">
-                    <p>8 hours</p>
-                </div>
-                <div class="text-gray-500  text-sm hover:text-white">
-                    <p>Beginner</p>
-                </div>
-            </div>
-        </div>
-        <div class="m-4 group"><img src="{{url('image/gallaryp3.png')}}" class="lg:w-[200px] md:w-[300px] rounded-2xl group-hover:scale-150 transition ease-out duration-200 group-hover:brightness-50">
-            <div class="hidden group-hover:flex flex-col absolute -bottom-80 left-[610px] space-y-2 p-2 w-72 shadow shadow-lg shadow-black">
-                <div class="bg-orange-600 py-1 px-2 rounded hover:bg-orange-400 text-white w-fit ">
-                    <p>Inertia</p>
-                </div>
-                <div class="text-gray-500  text-sm   hover:text-white">
-                    <p>8 hours</p>
-                </div>
-                <div class="text-gray-500  text-sm hover:text-white">
-                    <p>Beginner</p>
-                </div>
-            </div>
-        </div>
-        <div class="m-4 group"><img src="{{url('image/gallaryp4.png')}}" class="lg:w-[200px] md:w-[300px] rounded-2xl group-hover:scale-150 transition ease-out duration-200 group-hover:brightness-50">
-            <div class="hidden group-hover:flex flex-col absolute -bottom-80 right-32 space-y-2 p-2 w-72 shadow shadow-lg shadow-black">
-                <div class="bg-orange-600 py-1 px-2 rounded hover:bg-orange-400 text-white w-fit ">
-                    <p>LiveWire</p>
-                </div>
-                <div class="text-gray-500  text-sm   hover:text-white">
-                    <p>8 hours</p>
-                </div>
-                <div class="text-gray-500  text-sm hover:text-white">
-                    <p>Beginner</p>
-                </div>
-            </div>
-        </div>
+
     </div>
+
+
 </div>
+
+
