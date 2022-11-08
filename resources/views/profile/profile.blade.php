007a1a5e963d16e5070ef059862c93b7ac7a89ab
ati7697 <88265864+ati7697@users.noreply.github.com>
2022-11-08 14:26:42 +1000

    Update profile.blade.php
    change underline for link
    add email section

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<div class="w-screen text-white font-nav">
+<div class="w-screen text-white font-nav mb-36">
    <div class="flex flex-row m-10">
        <div ><img src="{{url('images/profilePic.jpg')}}" class="rounded-full w-[100px] ml-5"></div>
        <div class="flex flex-col m-10 space-y-3">
@@ -8,12 +8,15 @@
            <p class="text-xs">ati9776</p>
        </div>
    </div>
-    <div class="flex flex-row border-b-2 m-8 space-x-8 p-4  ">
-        <div><a href="{{url('profile')}}" class="ml-5 active:underline underline-offset-[20px] decoration-4  hover:text-[#7FEFEF] active:text-[#7FEFEF]">Profile</a></div>
-        <div><a href="" class="underline underline-offset-[20px] decoration-4  hover:text-[#7FEFEF] visited:text-[#7FEFEF]">My course</a></div>
+    <div class ="flex flex-row border-b-2 m-8 space-x-8 p-4  ">
+        <div><a href="{{url('profile')}}" class="ml-5 underline underline-offset-[20px] decoration-4  hover:text-[#7FEFEF] visited:text-[#7FEFEF]">Profile</a></div>
+        <div><a href="" >My course</a></div>
         <div><a href="#">My achievements</a></div>
     </div>
-
+    <div class="flex flex-row m-12 bg-gray-800 rounded-2xl p-5">
+        <div class="mr-3">Email:</div>
+        <div>ati@gmail.com</div>
+    </div>
 
 </div>
 @endsection
